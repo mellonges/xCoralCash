2adbb34468a271c4d8f6dad26855fc0b032bd5a6
404piano <404piano@gmail.com>
2021-10-28 13:31:32 +0300

    fix pauseOnFocus v2

--- pages/_app.js
+++ pages/_app.js
@@ -60,7 +60,7 @@ function MyApp({Component, pageProps}) {
                                 progress: undefined,
                                 pauseOnFocusLoss: false,
                             });
-                        toast.warn("disconnect in 15 seconds", {autoClose: 5000, pauseOnHover: false, pauseOnFocusLoss: false})
+                        toast.warn("disconnect in 15 seconds", {autoClose: 15000, pauseOnHover: false, pauseOnFocusLoss: false})
                        setTimeOudDisconnectId =  setTimeout(() => {
                                 dispatch(disconnectWallet())
                             }, 15000)
