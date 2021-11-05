d932d1fe73d724dfef12341d186cc44aa4dea4ab
404piano <404piano@gmail.com>
2021-11-05 17:33:09 +0300

    add table html

--- pages/futures/index.jsx
+++ pages/futures/index.jsx
@@ -30,6 +30,15 @@ const Index = () => {
                             <th className="text-right">MANAGE</th>
                         </tr>
                         </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                            <td>5</td>
+                        </tr>
+                        </tbody>
                     </Table>
                 </div>
             </MainLayout>
