3430ebd8614d70472c44603085021b03b3e1f758
twenty two <tod@mail.ru>
2021-11-05 18:27:28 +0400

    Fixed Footer.js - this is shit

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { Col, Container, Row, Button } from "reactstrap";
-import styles from "@/styles/components/Footer/Footer.module.scss";
+import styles from "../../styles/components/Footer/Footer.module.scss";
 
 const Footer = () => {
   const footerNavCol1 = [
@@ -18,49 +18,47 @@ const Footer = () => {
     {title: "Docs", url: "#"}
   ];
 
-
-
   return (
-      <footer className={styles.footerWrapper}>
-        <Container>
-          <div
-              className={`d-flex justify-content-between align-items-center ${styles.wrapper}`}
-          >
-            <div className={styles.footerLogo}>
-              xcoral.<span>cash</span>
-            </div>
-            <div className="footer-nav-block">
-              <nav className={styles.footerNav}>
-                {footerNavCol1.map((navItem, i) => (
-                    <Link key={i} href={navItem.url}>
-                      <a>{navItem.title}</a>
-                    </Link>
-                ))}
-              </nav>
-              <nav  className={styles.footerNav}>
-                {footerNavCol2.map((navItem, i) => (
-                    <Link key={i} href={navItem.url}>
-                      <a>{navItem.title}</a>
-                    </Link>
-                ))}
-              </nav>
-            </div>
+    <footer className={styles.footerWrapper}>
+      <Container>
+        <div
+          className={`d-flex justify-content-between align-items-center ${styles.wrapper}`}
+        >
+          <div className={styles.footerLogo}>
+            xcoral.<span>cash</span>
           </div>
-          <div className={styles.copyright}>
-            <span>xcoral.cash ©2021</span>
-            <Link href="https://d24va9fw68seps.cloudfront.net/hb_privacy.pdf">
-              <a className={styles.Link} target="_blank">
-                Privacy Notice
-              </a>
-            </Link>
-            <Link href="https://d24va9fw68seps.cloudfront.net/hb_terms.pdf">
-              <a className={styles.Link} target="_blank">
-                Terms of Service
-              </a>
-            </Link>
-          </div>
-        </Container>
-      </footer>
+
+          <nav className={styles.footerNav}>
+            {footerNavCol1.map((navItem, i) => (
+              <Link key={i} href={navItem.url}>
+                <a>{navItem.title}</a>
+              </Link>
+            ))}
+          </nav>
+          <nav  className={styles.footerNav}>
+            {footerNavCol2.map((navItem, i) => (
+                <Link key={i} href={navItem.url}>
+                  <a>{navItem.title}</a>
+                </Link>
+            ))}
+          </nav>
+        </div>
+
+        <div className={styles.copyright}>
+          <span>xcoral.cash ©2021</span>
+          <Link href="https://d24va9fw68seps.cloudfront.net/hb_privacy.pdf">
+            <a className={styles.Link} target="_blank">
+              Privacy Notice
+            </a>
+          </Link>
+          <Link href="https://d24va9fw68seps.cloudfront.net/hb_terms.pdf">
+            <a className={styles.Link} target="_blank">
+              Terms of Service
+            </a>
+          </Link>
+        </div>
+      </Container>
+    </footer>
   );
 };
 
