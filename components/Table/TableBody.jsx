e682c5ecb46585931584ef1f34401242096446e8
404piano <b89185236417@mail.ru>
2021-11-13 20:32:03 +0000

    add svg xCoral in Table

--- components/Table/TableBody.jsx
+++ components/Table/TableBody.jsx
@@ -1,126 +1,122 @@
 import React from 'react';
 import stylesFutures from "@/styles/pages/account/Futures/Futures.module.scss";
-import {Button} from "reactstrap";
+import { Button } from "reactstrap";
 import styles from "@/styles/pages/account/Wallet.module.scss";
 import stylesFuturesColor from "@/styles/pages/account/Futures/Futures.module.scss";
-import {useDispatch} from "react-redux";
-import {openAndCloseModalWindow, setActiveOperation} from "../../redux/reducers/rootReducer";
+import { useDispatch } from "react-redux";
+import { openAndCloseModalWindow, setActiveOperation } from "../../redux/reducers/rootReducer";
 
-const TableBody = ({nameCoin, disabled, expiration, APY}) => {
+const TableBody = ({ nameCoin, disabled, expiration, APY }) => {
     const imgLink = `https://d24va9fw68seps.cloudfront.net/coin_${nameCoin}.png`
     const dispatch = useDispatch()
     return (
         <>
             <tbody>
-            <tr>
-                <td className={stylesFutures.TDContentExp}>
-                    <div className={stylesFutures.TDContentExpCont}>
+                <tr>
+                    <td className={stylesFutures.TDContentExp}>
+                        <div className={stylesFutures.TDContentExpCont}>
 
-                        {/*<img  src={imgLink} alt=""/>*/}
+                            {/*<img  src={imgLink} alt=""/>*/}
 
-                        <div className={stylesFutures.TDTitle}>
-                            {nameCoin}
-                            <div className={stylesFutures.TDSubTitle}>
-                                expiration: {expiration}
+                            <div className={stylesFutures.TDTitle}>
+                                {nameCoin}
+                                <div className={stylesFutures.TDSubTitle}>
+                                    expiration: {expiration}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </td>
-                <td>
-                    {APY.toFixed(2)}% APY
-                </td>
-                <td>
-                    <div className={stylesFutures.OthTDTitle}>
-                        42,653.22
-                        <div className={stylesFutures.OthTDSubTitle}>
-                            DAI
-                        </div>
-                    </div>
-                </td>
-                <td className={stylesFutures.TDContentExp}>
-                    <div className={stylesFutures.TDContentExpCont}>
-                        <svg className={stylesFutures.OthTDImg} width="24" height="24" viewBox="0 0 24 24" fill="none"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 1L1 6.5L12 12L23 6.5L12 1Z" stroke="#658796" strokeWidth="1.936"
-                                  strokeLinecap="round" strokeLinejoin="round"></path>
-                            <path d="M1 17.5L12 23L23 17.5" stroke="#658796" strokeWidth="1.936" strokeLinecap="round"
-                                  strokeLinejoin="round"></path>
-                            <path d="M1 12L12 17.5L23 12" stroke="#658796" strokeWidth="1.936" strokeLinecap="round"
-                                  strokeLinejoin="round"></path>
-                        </svg>
+                    </td>
+                    <td>
+                        {APY.toFixed(2)}% APY
+                    </td>
+                    <td>
                         <div className={stylesFutures.OthTDTitle}>
-                            921.55
+                            42,653.22
                             <div className={stylesFutures.OthTDSubTitle}>
-                                Upcoming 1,240.32
+                                DAI
                             </div>
                         </div>
-                    </div>
-                </td>
-                <td className={stylesFutures.TDContentExp}>
-                    <div className={stylesFutures.TDContentExpCont}>
-                        <Button
-                            onClick={() => {
-                                dispatch(setActiveOperation(1))
-                                dispatch(openAndCloseModalWindow())
-                            }}
-                            color="primary"
-                            className={`d-flex ${styles.depositBtn}`}
-                        >
-                            <svg
-                                width="10"
-                                height="18"
-                                viewBox="0 0 10 18"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    d="M0.367122 6.13747C0.855001 5.66591 1.64412 5.66772 2.12969 6.14153L3.70256 7.68947L3.70256 1.21038C3.70256 0.541888 4.26054 4.92827e-07 4.94889 4.3265e-07C5.63724 3.72472e-07 6.19523 0.541888 6.19523 1.21038L6.19523 7.68953L7.76809 6.14147C8.25366 5.66766 9.04278 5.66585 9.53066 6.13741C10.0186 6.60904 10.0204 7.37539 9.5349 7.84919L5.8323 11.4755C5.34672 11.9481 4.55387 11.9507 4.06667 11.4766L0.362887 7.84925C-0.122561 7.37551 -0.120881 6.6091 0.367122 6.13747Z"
-                                    fill="white"
-                                />
-                                <rect
-                                    x="0.38623"
-                                    y="14.7241"
-                                    width="9.46484"
-                                    height="2.83945"
-                                    rx="1.41973"
-                                    fill="white"
-                                />
+                    </td>
+                    <td className={stylesFutures.TDContentExp}>
+                        <div className={stylesFutures.TDContentExpCont}>
+                            <svg width="26" height="22" viewBox="0 0 26 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="14.9702" cy="11" r="11" fill="#007585" fill-opacity="0.25" />
+                                <circle cx="11" cy="11" r="11" fill="#007585" />
+                                <path d="M10.78 11.592C9.596 13.264 9.004 14.404 9.004 15.012L6.832 15V14.736C7.32 14.472 7.84 13.952 8.392 13.176C9.232 11.984 9.8 11.168 10.096 10.728L9.112 9.228C8.216 7.852 7.536 7.064 7.072 6.864V6.6L9.784 6.612V6.876C9.784 7.148 9.872 7.46 10.048 7.812C10.248 8.212 10.608 8.796 11.128 9.564C12.104 8.18 12.592 7.192 12.592 6.6H14.704V6.864C14.456 6.992 14.224 7.168 14.008 7.392C13.8 7.616 13.452 8.06 12.964 8.724C12.476 9.38 12.084 9.936 11.788 10.392C13.156 12.4 13.996 13.596 14.308 13.98C14.628 14.356 14.908 14.608 15.148 14.736V15H12.46V14.736C12.46 14.608 12.412 14.452 12.316 14.268C12.228 14.084 12.152 13.936 12.088 13.824C12.032 13.704 11.98 13.604 11.932 13.524C11.884 13.444 11.82 13.34 11.74 13.212C11.66 13.076 11.6 12.972 11.56 12.9L10.78 11.592Z" fill="white" />
                             </svg>
-                            <span className="ml-auto mr-auto">Deposit</span>
-                        </Button>
-                        <Button 
-                            onClick={() => {
-                                dispatch(setActiveOperation(2))
-                                dispatch(openAndCloseModalWindow())
-                            }}
-                            className={`d-flex ${styles.depositBtn} ${stylesFuturesColor.RedeemActive}`}
-                            disabled={disabled}
-                        >
-                            <svg
-                                width="10"
-                                height="18"
-                                viewBox="0 0 10 18"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
+                            <div className={stylesFutures.OthTDTitle}>
+                                921.55
+                                <div className={stylesFutures.OthTDSubTitle}>
+                                    Upcoming 1,240.32
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                    <td className={stylesFutures.TDContentExp}>
+                        <div className={stylesFutures.TDContentExpCont}>
+                            <Button
+                                onClick={() => {
+                                    dispatch(setActiveOperation(1))
+                                    dispatch(openAndCloseModalWindow())
+                                }}
+                                color="primary"
+                                className={`d-flex ${styles.depositBtn}`}
                             >
-                                <path
-                                    d="M0.367122 6.13747C0.855001 5.66591 1.64412 5.66772 2.12969 6.14153L3.70256 7.68947L3.70256 1.21038C3.70256 0.541888 4.26054 4.92827e-07 4.94889 4.3265e-07C5.63724 3.72472e-07 6.19523 0.541888 6.19523 1.21038L6.19523 7.68953L7.76809 6.14147C8.25366 5.66766 9.04278 5.66585 9.53066 6.13741C10.0186 6.60904 10.0204 7.37539 9.5349 7.84919L5.8323 11.4755C5.34672 11.9481 4.55387 11.9507 4.06667 11.4766L0.362887 7.84925C-0.122561 7.37551 -0.120881 6.6091 0.367122 6.13747Z"
-                                    fill="white"
-                                />
-                                <rect
-                                    x="0.38623"
-                                    y="14.7241"
-                                    width="9.46484"
-                                    height="2.83945"
-                                    rx="1.41973"
-                                    fill="white"
-                                />
-                            </svg>
-                            <span className="ml-auto mr-auto">Redeem</span>
-                        </Button>
-                    </div>
-                </td>
-            </tr>
+                                <svg
+                                    width="10"
+                                    height="18"
+                                    viewBox="0 0 10 18"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M0.367122 6.13747C0.855001 5.66591 1.64412 5.66772 2.12969 6.14153L3.70256 7.68947L3.70256 1.21038C3.70256 0.541888 4.26054 4.92827e-07 4.94889 4.3265e-07C5.63724 3.72472e-07 6.19523 0.541888 6.19523 1.21038L6.19523 7.68953L7.76809 6.14147C8.25366 5.66766 9.04278 5.66585 9.53066 6.13741C10.0186 6.60904 10.0204 7.37539 9.5349 7.84919L5.8323 11.4755C5.34672 11.9481 4.55387 11.9507 4.06667 11.4766L0.362887 7.84925C-0.122561 7.37551 -0.120881 6.6091 0.367122 6.13747Z"
+                                        fill="white"
+                                    />
+                                    <rect
+                                        x="0.38623"
+                                        y="14.7241"
+                                        width="9.46484"
+                                        height="2.83945"
+                                        rx="1.41973"
+                                        fill="white"
+                                    />
+                                </svg>
+                                <span className="ml-auto mr-auto">Deposit</span>
+                            </Button>
+                            <Button
+                                onClick={() => {
+                                    dispatch(setActiveOperation(2))
+                                    dispatch(openAndCloseModalWindow())
+                                }}
+                                className={`d-flex ${styles.depositBtn} ${stylesFuturesColor.RedeemActive}`}
+                                disabled={disabled}
+                            >
+                                <svg
+                                    width="10"
+                                    height="18"
+                                    viewBox="0 0 10 18"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M0.367122 6.13747C0.855001 5.66591 1.64412 5.66772 2.12969 6.14153L3.70256 7.68947L3.70256 1.21038C3.70256 0.541888 4.26054 4.92827e-07 4.94889 4.3265e-07C5.63724 3.72472e-07 6.19523 0.541888 6.19523 1.21038L6.19523 7.68953L7.76809 6.14147C8.25366 5.66766 9.04278 5.66585 9.53066 6.13741C10.0186 6.60904 10.0204 7.37539 9.5349 7.84919L5.8323 11.4755C5.34672 11.9481 4.55387 11.9507 4.06667 11.4766L0.362887 7.84925C-0.122561 7.37551 -0.120881 6.6091 0.367122 6.13747Z"
+                                        fill="white"
+                                    />
+                                    <rect
+                                        x="0.38623"
+                                        y="14.7241"
+                                        width="9.46484"
+                                        height="2.83945"
+                                        rx="1.41973"
+                                        fill="white"
+                                    />
+                                </svg>
+                                <span className="ml-auto mr-auto">Redeem</span>
+                            </Button>
+                        </div>
+                    </td>
+                </tr>
             </tbody>
         </>
     );
