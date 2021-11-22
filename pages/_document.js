da05d791e7482d53fc8179f61c57afe213152ddb
404piano <404piano@gmail.com>
2021-11-22 18:00:44 +0300

    add browser navbar color

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="16x16" href="favicon/xcoral_icon_100px.png" />
           <link rel="apple-touch-icon" sizes="180x180" href="favicon/xcoral_icon_500px.png" />
           <link rel="manifest" href="site.webmanifest" />
-          <link rel="mask-icon" href="safari-pinned-tab.svg" color="003d56" />
+          <link rel="mask-icon" href="safari-pinned-tab.svg" color="45ffde" />
           <meta name="theme-color" content="aqua" />
           <script type="text/javascript" src="scripts/tawkio.js"></script>
         </Head>
