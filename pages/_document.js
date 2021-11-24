6d329c9b39145a5b04423aaa0349dc4dffcd0fc8
404piano <404piano@gmail.com>
2021-11-24 15:10:08 +0300

    fix favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/xcoral_icon_100px.ico" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/xcoral_icon_100px.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/xcoral_icon_500px.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/xcoral_icon_100px.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/xcoral_icon_100px.png" />
           <link rel="mask-icon" href="safari-pinned-tab.svg" color="45ffde" />
           <meta name="theme-color" content="45ffde" />
         </Head>
